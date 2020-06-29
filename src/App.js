5c04cc7e22c51f92c825c3223aed330dca511577
snufka <52801250+snufka@users.noreply.github.com>
2020-06-29 21:33:55 +0200

    creating card for search result

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1>hello worlds</h1>
+      <Card />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <h3>Launch Data</h3>
+      <p>Launch Status: Successful/ Failed / Future</p>
+      <p>Date: </p>
+      <p>Details: </p>
+      <p>Rocket info: </p>
+      <img src="./public/img/rocket.jpg"></img>
+    </div>
+  );
+}
