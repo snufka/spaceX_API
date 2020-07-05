cf91b1250fa3abd3084400e63ab2d6e6b8399fb8
snufka <52801250+snufka@users.noreply.github.com>
2020-07-05 23:33:15 +0200

    functioning search bar?

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ function App() {
   return (
     <div>
       <h1>Space X Launches</h1>
-      <div className="form-group">
-        <form>
-          <label className="col-sm-2 col-form-label">Search:</label>
-          <input type="search" class="col-sm-8"></input>
-        </form>
-      </div>
       <LaunchesList />
       <Card />
     </div>
