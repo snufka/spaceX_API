b6a0a7e6c1f90ed1ce082afbda39b0ad50069e11
snufka <52801250+snufka@users.noreply.github.com>
2020-07-21 22:02:13 +0200

    mapping over laumches

--- src/Search.js
+++ src/Search.js
@@ -4,22 +4,24 @@ export default function Search() {
   //state inpute
   const [query, setQuery] = useState("67");
   //state api result
+  const [launches, setLaunches] = useState([]);
 
   const searchApi = async (e) => {
     e.preventDefault();
     console.log("submitting");
 
-    const url = "https://api.spacexdata.com/v3/launches?limit=20?" + query;
+    const url = "https://api.spacexdata.com/v3/launches/" + query;
     try {
       const res = await fetch(url);
       const data = await res.json();
       console.log(data);
+      setLaunches(data);
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <div>
+    <>
       <form onSubmit={searchApi}>
         <label className="label" htmlFor="query">
           Api Search
@@ -28,7 +30,7 @@ export default function Search() {
           className="input"
           type="text"
           name="query"
-          placeholder="flight number"
+          placeholder="Flight Number"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
@@ -36,6 +38,13 @@ export default function Search() {
           Search
         </button>
       </form>
-    </div>
+      <div className="card-list">
+        {launches.map((launch) => (
+          <div>
+            <h1> {launch.flight_number} </h1>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
