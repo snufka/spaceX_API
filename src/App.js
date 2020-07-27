b3e2f93007619d05671197234d0dfd1d1b77a64a
snufka <52801250+snufka@users.noreply.github.com>
2020-07-27 21:49:00 +0200

    header update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import Search from "./Search";
 
 function App() {
   return (
-    <div className="col-sm-6 text-center">
-      <h1 className="text-info">Space X Launches</h1>
-      <Search />
-    </div>
+    <>
+      <div className="media">
+        <img
+          src="https://www.spacex.com/static/images/backgrounds/anasisii_website_desktop3.jpg"
+          className="align-self-end mr-3"
+          alt="parallax"
+        ></img>
+      </div>
+      <div className="col-sm-6 text-center">
+        <h1 className="text-info">Space X Launches</h1>
+        <Search />
+      </div>
+    </>
   );
 }
 
