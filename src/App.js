78294fa3d3d864196fee69f4d4f0171f5d16f86e
snufka <52801250+snufka@users.noreply.github.com>
2020-07-29 15:30:57 +0200

    webdisgin

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Search from "./Search";
 function App() {
   return (
     <>
-      <div className="media">
-        <img
-          src="https://www.spacex.com/static/images/backgrounds/anasisii_website_desktop3.jpg"
-          className="align-self-end mr-3"
-          alt="parallax"
-        ></img>
-      </div>
       <div className="col-sm-10 text-center">
         <h1 className="text-info">Space X Launches</h1>
+        <div className="parallax">
+          <img
+            src="https://www.spacex.com/static/images/backgrounds/anasisii_website_desktop3.jpg"
+            className="align-self-end mr-3"
+            alt="parallax"
+          ></img>
+        </div>
         <Search />
       </div>
     </>
